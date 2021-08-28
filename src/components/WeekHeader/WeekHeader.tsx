4a9cf7a9b5eea88a7640cd04f13ce1edac3dbf4f
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2021-08-28 18:28:44 +0530

    Added time slots in table body with basic styling

--- src/components/WeekHeader/WeekHeader.tsx
+++ src/components/WeekHeader/WeekHeader.tsx
@@ -6,7 +6,8 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { getAllDaysInTheWeek } from '../../utils';
+import { getAllDaysInTheWeek, times } from '../../utils';
+import moment from 'moment';
 
 export interface Day {
 	date: number;
@@ -18,36 +19,72 @@ const useStyles = makeStyles({
 	table: {
 		minWidth: 650,
 	},
+	textCenter: {
+		textAlign: 'center',
+	},
+	pt0: {
+		paddingTop: '0px',
+	},
+	date: {
+		fontSize: '26px',
+		color: '#70757a',
+		marginTop: '10px',
+	},
+	mt5px: {
+		marginTop: '5px',
+	},
+	time: {
+		color: '#70757a',
+		width: '10px',
+	},
 });
 
 const WeekHeader = () => {
 	const classes = useStyles();
 	const daysInWeek = getAllDaysInTheWeek();
+	const formattedTime = (time: number) => moment().set('hours', time).format('h a');
 	return (
 		<TableContainer component={Paper}>
 			<Table>
 				<TableHead>
 					<TableRow>
-						{daysInWeek.map((day) => (
-							<TableCell style={{ textAlign: 'center' }}>
-								<p>{day.weekDayName}</p>
-								<p>{day.date}</p>
+						{daysInWeek.map((day: Day) => (
+							<TableCell key={day.date} className={classes.textCenter}>
+								<div>{day.weekDayName}</div>
+								<div className={classes.date}>{day.date}</div>
 							</TableCell>
 						))}
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					{/* {rows.map((row) => (
-						<TableRow key={row.name}>
-							<TableCell component='th' scope='row'>
-								{row.name}
+					{times.map((time: any) => (
+						<TableRow key={time}>
+							<TableCell>
+								<span>{formattedTime(time)}</span>
 							</TableCell>
-							<TableCell align='right'>{row.calories}</TableCell>
-							<TableCell align='right'>{row.fat}</TableCell>
-							<TableCell align='right'>{row.carbs}</TableCell>
-							<TableCell align='right'>{row.protein}</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
+							<TableCell>
+								<div>text</div>
+							</TableCell>
 						</TableRow>
-					))} */}
+					))}
 				</TableBody>
 			</Table>
 		</TableContainer>
